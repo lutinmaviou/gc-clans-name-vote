fa8a2b0436398096b7e54caf7b799ff09339219c
Bertrand Bourion <bertrandbou@hotmail.fr>
2022-11-03 19:22:41 +0100

    style: choose next/image for stretching on background

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { NextComponentType } from 'next';
+import Image from 'next/image';
+import Bg from '../../public/img/bg.jpg';
 
 const HomePage: NextComponentType = () => {
   const [ip, setIP] = useState('');
@@ -18,7 +20,22 @@ const HomePage: NextComponentType = () => {
     <section className="App">
       <h1>Team France 974</h1>
       <h2>Oté zot tout !</h2>
-      <p className="question">Dois-t'on changer le nom du clan ?</p>
+      <div className="bg">
+        <Image src={Bg} fill alt="Fond d'écran golfeur" />
+      </div>
+      <div className="question">
+        <p>Dois-t'on changer le nom du clan ?</p>
+        <div className="inputs">
+          <div className="checkbox1">
+            <label htmlFor="">OUI</label>
+            <input type="checkbox" />
+          </div>
+          <div className="checkbox2">
+            <label htmlFor="">NON</label>
+            <input type="checkbox" />
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
