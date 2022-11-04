91f215a6300e9b6d3e1f5fc932621b09eb088e89
Bertrand Bourion <bertrandbou@hotmail.fr>
2022-11-04 17:36:02 +0100

    style: style all the document

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -10,6 +10,15 @@ const HomePage: NextComponentType = () => {
     e.preventDefault();
     console.log(e.target);
   };
+  const handleCheckbox = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const yes: HTMLInputElement | null = document.querySelector('#yes');
+    const no: HTMLInputElement | null = document.querySelector('#no');
+    if (yes != null && no != null && no.checked === true) {
+      yes.checked = false;
+    } else if (no != null && yes != null && yes.checked === true) {
+      no.checked = false;
+    }
+  };
   //creating function to load ip address from the API
   const getData = async () => {
     const res = await axios.get('https://geolocation-db.com/json/');
@@ -29,15 +38,23 @@ const HomePage: NextComponentType = () => {
       <form className="question">
         <p>Dois-t'on changer le nom du clan ?</p>
         <div className="inputs">
-          <div className="checkbox1">
-            <label htmlFor="yes">OUI</label>
-            <input type="checkbox" name="yes" id="yes" />
-          </div>
-          <div className="checkbox2">
+          <div className="checkbox">
             <label htmlFor="no">NON</label>
-            <input type="checkbox" name="no" id="no" />
+            <input type="checkbox" name="no" id="no" onChange={handleCheckbox} />
           </div>
           <button onClick={handleSubmit}>Valider</button>
+        </div>
+      </form>
+      <hr />
+      <p className="purpose">Sinon fais ta proposion ! (16 caractères max)</p>
+      <hr />
+      <form className="else">
+        <div className="inputs2">
+          <div className="clansName">
+            <label htmlFor="clansName">Nom du clan</label>
+            <input type="text" name="clansName" id="clansName" />
+          </div>
+          <button onClick={handleSubmit} id="submit">Valider</button>
         </div>
       </form>
     </section>
